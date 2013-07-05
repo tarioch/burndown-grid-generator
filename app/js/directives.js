a2b05db86f6222e890d5f68a9462f4fee9002324
Patrick Ruckstuhl <patrick.ruckstuhl@zuehlke.com>
2013-07-05 11:46:59 +0200

    Working interaction between data entry and grid

--- app/js/directives.js
+++ app/js/directives.js
@@ -3,24 +3,31 @@ angular.module('burndownGridGenerator.directives', [])
 	.directive('burndownGrid', [function(){
 		'use strict';
 		
-//		var link  = function(scope, element, attrs, ctrl) {
 		var link  = function(scope, element) {
-			var chart = d3.select(element[0])
-				.append('svg:svg')
-				.attr('width', 400)
-				.attr('height', 300);
-
-			chart.append('svg:rect')
-				.attr('x', 100)
-				.attr('y', 100)
-				.attr('height', 100)
-				.attr('width', 200);
+			
+			scope.update = function(){
+				d3.select(element[0]).select('svg').remove();
+				
+				var chart = d3.select(element[0])
+					.append('svg:svg')
+					.attr('width', 400)
+					.attr('height', 300);
+	
+				chart.append('svg:rect')
+					.attr('x', 100)
+					.attr('y', 100)
+					.attr('height', 100)
+					.attr('width', 10 * scope.days);
+			};
+			
+			scope.$watch('maxHours', scope.update);
+			scope.$watch('days', scope.update);
 		};
 		
 		return {
 			replace: true,
 			restrict: 'E',
-			scope: {data: '=', options: '='},
+			scope: {maxHours: '=', days: '='},
 			template: '<div></div>',
 			link: link
 		};
