77cab5bbecb65b9f525dfb02dad36f0ca0b93312
Patrick Ruckstuhl <patrick.ruckstuhl@zuehlke.com>
2013-07-05 13:45:38 +0200

    working x and y axis

--- app/js/directives.js
+++ app/js/directives.js
@@ -10,24 +10,38 @@ angular.module('burndownGridGenerator.directives', [])
 
 				var width = 600;
 				var height = 300;
+				var padding = 80;
 				
 				var chart = d3.select(element[0])
 					.append('svg:svg')
-					.attr('width', width)
-					.attr('height', height);
+					.attr('width', width + padding)
+					.attr('height', height + padding);
 				
-				var xAxisScale = d3.scale.linear().
-					domain([0, scope.days]).
-					range([0, width]);
+				var xAxisScale = d3.scale.linear()
+					.domain([0, scope.days])
+					.range([0, width]);
+
+				var yAxisScale = d3.scale.linear()
+					.domain([0, scope.maxHours])
+					.range([height, 0]);
 
 				var xAxis = d3.svg.axis()
 					.scale(xAxisScale)
-					.orient('bottom')
-					;
+					.orient('bottom');
+				
+				var yAxis = d3.svg.axis()
+					.scale(yAxisScale)
+					.orient('left');
 
 				chart.append('g')
 					.attr('class', 'x axis')
+					.attr('transform', 'translate(40, ' + height + ')')
 					.call(xAxis);
+			
+				chart.append('g')
+					.attr('class', 'y axis')
+					.attr('transform', 'translate(40, 0)')
+					.call(yAxis);
 			};
 			
 			scope.$watch('maxHours', scope.update);
