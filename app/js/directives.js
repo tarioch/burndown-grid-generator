b6c300154594c05bebf90c63478a589c983cb9bd
Patrick Ruckstuhl <patrick.ruckstuhl@zuehlke.com>
2013-07-05 13:25:49 +0200

    draw x axis

--- app/js/directives.js
+++ app/js/directives.js
@@ -7,17 +7,27 @@ angular.module('burndownGridGenerator.directives', [])
 			
 			scope.update = function(){
 				d3.select(element[0]).select('svg').remove();
+
+				var width = 600;
+				var height = 300;
 				
 				var chart = d3.select(element[0])
 					.append('svg:svg')
-					.attr('width', 400)
-					.attr('height', 300);
-	
-				chart.append('svg:rect')
-					.attr('x', 100)
-					.attr('y', 100)
-					.attr('height', 100)
-					.attr('width', 10 * scope.days);
+					.attr('width', width)
+					.attr('height', height);
+				
+				var xAxisScale = d3.scale.linear().
+					domain([0, scope.days]).
+					range([0, width]);
+
+				var xAxis = d3.svg.axis()
+					.scale(xAxisScale)
+					.orient('bottom')
+					;
+
+				chart.append('g')
+					.attr('class', 'x axis')
+					.call(xAxis);
 			};
 			
 			scope.$watch('maxHours', scope.update);
